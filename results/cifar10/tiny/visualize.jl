2b7f30d8098544c543515d072a6142cb06d945f8
Avik Pal <avikpal@mit.edu>
2022-05-19 13:37:44 -0400

    Dump CIFAR10 tiny results

--- results/cifar10/tiny/visualize.jl
+++ results/cifar10/tiny/visualize.jl
@@ -22,18 +22,20 @@ df = Pandas.concat(dfs)
 
 Seaborn.seaborn.set(; font_scale=1.5)
 
+df = Pandas.rename(df, columns=Dict("Test/Batch Time" => "Test/Batch Time (s)", "Test/Accuracy" => "Test/Accuracy (%)", "Test/Residual" => "Test/Residual Norm", "Train/Batch Time" => "Train/Batch Time (s)", "Train/Accuracy" => "Train/Accuracy (%)", "Train/Residual" => "Train/Residual Norm"))
+
 df_melt = Pandas.melt(
     df;
     id_vars=["Epoch", "Model Type"],
     value_vars=[
-        "Test/Accuracy",
+        "Test/Accuracy (%)",
         "Test/NFE",
-        "Test/Batch Time",
-        "Test/Residual",
-        "Train/Accuracy",
+        "Test/Batch Time (s)",
+        "Test/Residual Norm",
+        "Train/Accuracy (%)",
         "Train/NFE",
-        "Train/Batch Time",
-        "Train/Residual",
+        "Train/Batch Time (s)",
+        "Train/Residual Norm",
     ],
     var_name="Metric Full",
     value_name="Value",
@@ -43,6 +45,6 @@ _splitted = split.(df_melt["Metric Full"], "/")
 df_melt["Metric"] = last.(_splitted)
 df_melt["Mode"] = first.(_splitted)
 
-plt = Seaborn.seaborn.relplot(x="Epoch", y="Value", data=df_melt, hue="Model Type", col="Metric", row="Mode", facet_kws=Dict("sharey" => false, "legend_out" => false, "margin_titles" => true), kind="line");
+plt = Seaborn.seaborn.relplot(x="Epoch", y="Value", data=df_melt, hue="Model Type", col="Metric", row="Mode", row_order=["Train", "Test"], facet_kws=Dict("sharey" => false, "legend_out" => false, "margin_titles" => true), kind="line");
 Seaborn.plt.tight_layout()
 plt.savefig("cifar10-tiny.pdf", bbox_inches="tight")
